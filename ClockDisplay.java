927af6862b29cc24e2edfa4a52a1d4b51b5d4998
Erik <46272257+ecooke1970@users.noreply.github.com>
2019-09-23 23:38:29 -0400

    Changed to display 12 hour clock

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -17,15 +17,17 @@ public class ClockDisplay
     private NumberDisplay hours;
     private NumberDisplay minutes;
     private String displayString;    // simulates the actual display
-    
+    private String ampm;
+
     /**
      * Constructor for ClockDisplay objects. This constructor 
-     * creates a new clock set at 00:00.
+     * creates a new clock set at 12:00AM.
      */
     public ClockDisplay()
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(12);
         minutes = new NumberDisplay(60);
+        ampm = "AM";
         updateDisplay();
     }
 
@@ -34,11 +36,12 @@ public class ClockDisplay
      * creates a new clock set at the time specified by the 
      * parameters.
      */
-    public ClockDisplay(int hour, int minute)
+    public ClockDisplay(int hour, int minute, String ampm)
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(12);
         minutes = new NumberDisplay(60);
-        setTime(hour, minute);
+        setAmpm(ampm);
+        setTime(hour, minute, ampm);
     }
 
     /**
@@ -50,6 +53,17 @@ public class ClockDisplay
         minutes.increment();
         if(minutes.getValue() == 0) {  // it just rolled over!
             hours.increment();
+            if(hours.getValue() == 0)
+            {
+                if(ampm == "AM")
+                {
+                    setAmpm("PM");
+                }
+                else
+                {
+                    setAmpm("AM");
+                }
+            }            
         }
         updateDisplay();
     }
@@ -58,10 +72,11 @@ public class ClockDisplay
      * Set the time of the display to the specified hour and
      * minute.
      */
-    public void setTime(int hour, int minute)
+    public void setTime(int hour, int minute, String ampm)
     {
         hours.setValue(hour);
         minutes.setValue(minute);
+        setAmpm(ampm);
         updateDisplay();
     }
 
@@ -72,13 +87,37 @@ public class ClockDisplay
     {
         return displayString;
     }
-    
+
     /**
      * Update the internal string that represents the display.
      */
     private void updateDisplay()
     {
-        displayString = hours.getDisplayValue() + ":" + 
-                        minutes.getDisplayValue();
+        if(hours.getValue() == 0)
+        {
+            displayString = "12" + ":" + minutes.getDisplayValue() + ampm;
+        }
+        else
+        {
+            displayString = hours.getValue() + ":" + minutes.getDisplayValue() + ampm;
+        }
     }
+
+    /**
+     * Checks if AM or am has been entered for time of day and sets the ampm variable.
+     * If anything else is entered ampm is set to PM.
+     */
+    private void setAmpm(String ampm)
+    {
+        if(ampm == "AM" || ampm == "am" || ampm == "Am" || ampm == "aM")
+        //if(ampm.toUpperCase() == "AM")
+        {
+            this.ampm = "AM";
+        }
+        else
+        {
+            this.ampm = "PM";
+        }
+    }
+
 }
